1ddcc4c2993a2b499a2ff6dbf15b1e2cae4e4c2c
ogimly <ogimly@gmail.com>
2022-06-07 03:30:17 +0500

    feat: generate portfolio page

---
+++ src/scripts/data.js
@@ -0,0 +1,36 @@
+const data = {
+  item0: {
+    id: 'virtual-keyboard',
+    title: 'Virtual keyboard',
+
+    description: 'You should to create a virtual keyboard:',
+    descriptionList: {
+      item0: 'all the necessary elements are generated with the usage of JS',
+      item1:
+        'pressing a key on a physical keyboard highlights the key on the virtual keyboard',
+      item2: 'keystrokes are animated',
+      item3: 'the virtual keyboard is able to switch between two language layouts',
+      item4:
+        'clicks on the buttons with a mouse on the virtual keyboard and pressing keys on a physical keyboard should input symbols to the text area located on the page above the virtual keyboard',
+    },
+
+    descriptionLink:
+      'https://github.com/rolling-scopes-school/tasks/blob/master/tasks/virtual-keyboard/virtual-keyboard-en.md',
+    repositoryLink: 'https://github.com/ogimly/virtual-keyboard',
+    deployLink: 'https://ogimly.github.io/virtual-keyboard',
+    imgLink:
+      'https://user-images.githubusercontent.com/101447709/167273586-cd8760e2-82d4-404c-ac60-eb42379a38d5.png',
+    imgAlt: 'Virtual keyboard screen',
+
+    learn: 'In this task, I learned:',
+    learnList: {
+      item0: 'generation of elements in DOM',
+      item1: 'handling and catching events',
+      item2: 'installing and configuring Webpack',
+      item3: 'installing and configuring ESLint and Prettier',
+      item4: 'writing a Pull Request wish guideline',
+    },
+  },
+};
+
+export default data;
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,7 @@
-window.onload = () => {
+import data from './data';
+
+//* BURGER *//
+const addBurgerHandler = () => {
   const burger = document.getElementById('burger');
   if (burger)
     burger.addEventListener('click', () => {
@@ -11,4 +14,112 @@ window.onload = () => {
       burger.classList.remove('burger_open');
     });
   });
+};
+
+//* TASKS *//
+const addPToDOM = (section, text) => {
+  const p = document.createElement('p');
+  p.className = 'text tasks-section__text';
+  p.innerHTML = text;
+  section.appendChild(p);
+};
+
+const addListToDOM = (section, list) => {
+  const ul = document.createElement('ul');
+  ul.className = 'tasks-section__list';
+  section.appendChild(ul);
+
+  Object.values(list).forEach((el) => {
+    const li = document.createElement('li');
+    li.className = 'tasks-section__item';
+    li.innerHTML = el;
+    ul.appendChild(li);
+  });
+};
+
+const addLinkToDOM = (section, text, link) => {
+  const a = document.createElement('a');
+  a.className = 'tasks-section__link';
+  a.target = '_blank';
+  a.href = link;
+  a.innerHTML = text;
+  section.appendChild(a);
+};
+
+const addImgToDOM = (section, text, link) => {
+  const img = document.createElement('img');
+  img.className = 'tasks__img';
+  img.src = link;
+  img.alt = text;
+  section.appendChild(img);
+};
+
+const addTasksToDOM = (tasks, taskData) => {
+  Object.values(taskData).forEach((el) => {
+    const section = document.createElement('section');
+    section.className = `tasks-section ${el.id}`;
+    section.id = el.id;
+
+    const h2 = document.createElement('h2');
+    h2.className = 'title tasks-section__title';
+    h2.innerHTML = `${el.title}:`;
+    section.appendChild(h2);
+
+    addPToDOM(section, el.description);
+    let list = el.descriptionList;
+    addListToDOM(section, list);
+
+    const nav = document.createElement('nav');
+    nav.className = 'tasks-section__nav';
+    section.appendChild(nav);
+
+    addLinkToDOM(nav, 'Full description', el.descriptionLink);
+    addLinkToDOM(nav, 'View repository', el.repositoryLink);
+    addLinkToDOM(nav, 'Open deploy', el.deployLink);
+
+    addImgToDOM(section, el.imgAlt, el.imgLink);
+
+    addPToDOM(section, el.learn);
+    list = el.learnList;
+    addListToDOM(section, list);
+
+    tasks.appendChild(section);
+  });
+};
+
+const addTaskListToDOM = (section, taskData) => {
+  const ul = document.createElement('ul');
+  ul.className = 'sidebar-section__list';
+  section.appendChild(ul);
+
+  Object.values(taskData).forEach((el) => {
+    const li = document.createElement('li');
+    li.className = 'sidebar-section__item tasks__item';
+
+    const a = document.createElement('a');
+    a.className = 'sidebar-section__link task-list__item';
+    a.href = `#${el.id}`;
+    a.innerHTML = el.title;
+    li.appendChild(a);
+
+    ul.appendChild(li);
+  });
+};
+
+const generateTasks = () => {
+  const taskList = document.getElementById('task-list');
+  if (taskList) {
+    addTaskListToDOM(taskList, data);
+  }
+
+  const tasks = document.getElementById('tasks');
+  if (tasks) {
+    addTasksToDOM(tasks, data);
+  }
+};
+
+window.onload = () => {
+  addBurgerHandler();
+
+  generateTasks();
 };
